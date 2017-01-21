27fb1efe88f719cf2a779761e4eb690d673599fb
Samuel Shih <sam00429@gmail.com>
2017-01-20 20:42:00 -0500

    remove users

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' }); // render index.ejs
+});
+
+
+router.get('/test', function(req, res, next) {
+  res.render('index', { title: 'test' }); // render index.ejs
 });
 
 module.exports = router;
