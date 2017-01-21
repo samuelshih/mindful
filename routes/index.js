7bad8d4a200537340ffca5ebc828a4058343777a
Samuel Shih <sam00429@gmail.com>
2017-01-21 00:02:33 -0500

    test graph

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var watson = require('watson-developer-cloud');
+var alchemy_language = watson.alchemy_language({
+  api_key: '5c96e6bf5474d65eabcf30283a33118672a66a6d'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' }); // render index.ejs
+  console.log(res)
+  res.render('index', { title: 'helo world' }); // render index.ejs
 });
 
-
-router.get('/test', function(req, res, next) {
-  res.render('index', { title: 'test' }); // render index.ejs
+router.get('/graph', function(req, res, next) {
+  res.render('graph', { title: 'Graph'}); // render index.ejs
 });
 
 module.exports = router;
