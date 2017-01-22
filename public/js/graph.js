e0a88aca40266390c5f581089d4bb65bf2755322
charsaki <ciwasak1@swarthmore.edu>
2017-01-21 21:57:43 -0500

    process data info fixed

--- public/js/graph.js
+++ public/js/graph.js
@@ -19,49 +19,64 @@ var blank = "rgba(0, 0, 0, 0)"
 // *** LINE GRAPH ******
 // *********************
 
-var lineData = {
-  labels: hours,
-  datasets: [{
-    label: 'Anger',
-    data: socket_data/*[12, 19, 3, 17, 6, 3, 7, 5, 3, 1, 3, 7, 5, 3, 2, 4, 15, 37]*/,
-    borderColor: emotionsColors['Anger'],
-    backgroundColor: blank
-  }, {
-    label: 'Disgust',
-    data: [2, 29, 5, 5, 2, 3, 10, 16, 3 , 5, 31, 21, 9],
-    borderColor: emotionsColors['Disgust'],
-    backgroundColor: blank
-  }]
+// var lineData = {
+//   labels: hours,
+//   datasets: [{
+//     label: 'Anger',
+//     data: socket_data/*[12, 19, 3, 17, 6, 3, 7, 5, 3, 1, 3, 7, 5, 3, 2, 4, 15, 37]*/,
+//     borderColor: emotionsColors['Anger'],
+//     backgroundColor: blank
+//   }, {
+//     label: 'Disgust',
+//     data: [2, 29, 5, 5, 2, 3, 10, 16, 3 , 5, 31, 21, 9],
+//     borderColor: emotionsColors['Disgust'],
+//     backgroundColor: blank
+//   }]
+// }
+
+var feels = new Array(5);
+for (var i = 0; i < 5; i++){
+  feels[i] = new Array(24);
+  for (var hour = 0; hour < 24; hour++){
+    var key = '2017_1_1_' + String(hour);
+    if (key in socket_data){
+      feels[i][hours] = Number(socket_data[key]*100;
+    }
+    else{
+      feels[i][hours] = 0.0
+    }
+  }
 }
 
-//
-// var lineData = {
-//   labels = hours,
-//   datasets = [{
-//       label: 'Anger',
-//       borderColor: emotionsColors['Anger'],
-//       backgroundColor: blank
-//     }, {
-//       label: 'Disgust',
-//       borderColor: emotionsColors['Disgust'],
-//       backgroundColor: blank
-//     }, {
-//       label: 'Fear',
-//       borderColor: emotionsColors['Fear'],
-//       backgroundColor: blank
-//     }, {
-//       label: 'Joy',
-//       borderColor: emotionsColors['Joy'],
-//       backgroundColor: blank
-//     }, {
-//       label: 'Sadness',
-//       borderColor: emotionsColors['Sadness'],
-//       backgroundColor: blank
-//     }]
-// }
-//
-//
-
+var lineData = {
+  labels = hours,
+  datasets = [{
+      label: 'Anger',
+      data = feels[0],
+      borderColor: emotionsColors['Anger'],
+      backgroundColor: blank
+    }, {
+      label: 'Disgust',
+      data = feels[1],
+      borderColor: emotionsColors['Disgust'],
+      backgroundColor: blank
+    }, {
+      label: 'Fear',
+      data = feels[2],
+      borderColor: emotionsColors['Fear'],
+      backgroundColor: blank
+    }, {
+      label: 'Joy',
+      data = feels[3],
+      borderColor: emotionsColors['Joy'],
+      backgroundColor: blank
+    }, {
+      label: 'Sadness',
+      data = feels[0],
+      borderColor: emotionsColors['Sadness'],
+      backgroundColor: blank
+    }]
+}
 
 var lineCtx = document.getElementById('emotionsLineChart').getContext('2d');
 var lineChart = new Chart(lineCtx, {
